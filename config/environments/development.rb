da27d38f272565b7c34f74b2e294617346be59d2
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-05-14 16:39:57 -0700

    Change rails port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
+  config.debug_exception_response_format = :api
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
